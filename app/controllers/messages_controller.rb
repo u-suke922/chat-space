3161299039d969e069ab24b01b649a9d0b342da8
u-suke922 <eyrui.ctakemuri@icloud.com>
2020-01-07 17:22:07 +0900

    chat-space.view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
+  # before_action :set_group
+
   def index
+    # @message = Message.new
+    # @messages = @group.messages.includes(:user)
   end
+
+  # private
+
+  # def set_group
+  #   @group = Group.find(params[:group_id])
+  # end
 end
