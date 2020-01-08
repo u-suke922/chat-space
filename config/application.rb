a0f7430d2d874738d77e93915c798ac06f495b76
u-suke922 <eyrui.ctakemuri@icloud.com>
2020-01-08 17:22:22 +0900

    user機能

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,11 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
+    config.i18n.default_locale = :ja
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.test_framework false
+      g.test_framework false     
     end
   end
 end
